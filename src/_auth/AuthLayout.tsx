6d175cb61fd2f949cbad23392d638d2be463174e
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-12-07 18:26:15 +0100

    little modification

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -15,7 +15,7 @@ const AuthLayout = () => {
       <section className='flex flex-1 justify-center items-center flex-col py-10'>
         <Outlet/>
       </section>*
-      <img src='/public/assets/images/sideimage.png'
+      <img src='src/public/assets/images/sideimage.png'
       alt='logo'
       className='hidden xl:block h-screen w-1/2 object-cover bg-no repeat'/>
       </>
