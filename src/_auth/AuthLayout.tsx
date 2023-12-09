683abe1823625cec47850edd265e1fdbab5b3eb9
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-12-09 23:16:43 +0100

    Form sign up completed

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,11 +14,13 @@ const AuthLayout = () => {
       <>
       <section className='flex flex-1 justify-center items-center flex-col py-10'>
         <Outlet/>
-      </section>*
-      <img src='src/public/assets/images/sideimage.png'
+      </section>
+      <img src='../../assets/images/sideimage.png'
       alt='logo'
-      className='hidden xl:block h-screen w-1/2 object-cover bg-no repeat'/>
+      className='xl:block h-screen w-1/2 object-cover bg-no repeat'/>
+    
       </>
+   
     )  }
     </>
   )
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,138 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
-const SignupForm = () => {
-  return (
-    <div><Button>Click me</Button></div>
-  )
-}
+import React from "react";
 
-export default SignupForm
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { SignupValidation } from "@/lib/validation";
+import { Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const SignupForm = () => {
+  const isLoading = false;
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.png" alt="logo" width="171" height="36" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+      
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use Snapgram enter your account details
+        </p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4 ">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+               
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+<FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+               
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+<FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+               
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+<FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+               
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="shad-button_primary">
+            {
+              isLoading?(
+
+                <div className="flex-center gap-2">
+<Loader/>Loading ...
+
+                </div>
+              ):"Sign up"
+
+            }
+
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+Already have an account
+<Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SignupForm;
