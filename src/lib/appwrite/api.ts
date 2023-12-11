c9f6365f5dec91912e671664171fda806e05d399
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-12-11 19:21:23 +0100

    correct bug

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -13,6 +13,7 @@ export async function createUserAccount(user:INewUser) {
         user.name
         
         )
+        return newAccount;
     } catch (error) {
         console.log(error);
         return error;
