01b604ee84a25771d3b1114909da67e082507bca
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-12-14 17:56:58 +0100

    starting database design

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,6 @@ ReactDOM .createRoot(document.getElementById('root')!).render(
 <App/>
 </BrowserRouter>
 // VITE_APPWRITE_URL
-
+//VITE_APPWRITE_STORAGE_ID='657ae65c4d5a2528937d'
+//VITE_APPWRITE_DATABASE_ID='657ae6af89a9019eb99a'
 )
